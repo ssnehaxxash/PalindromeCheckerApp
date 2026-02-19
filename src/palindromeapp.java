b81c203e20828a8ae9fa3c6c794cbbd24dc2bab8
ssnehaxxash <snehasunilpathak@gmail.com>
2026-02-19 09:27:19 +0530

    Displaying welcome message

--- src/palindromeapp.java
+++ src/palindromeapp.java
@@ -4,25 +4,8 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Welcome to Palindrome Checker");
-
-        System.out.print("Enter a word: ");
-        String word = sc.nextLine();
-
-        String reverse = "";
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reverse = reverse + word.charAt(i);
-        }
-
-        if (word.equalsIgnoreCase(reverse)) {
-            System.out.println("It is a Palindrome");
-        } else {
-            System.out.println("It is NOT a Palindrome");
-        }
-
-        sc.close();
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully");
     }
 }
