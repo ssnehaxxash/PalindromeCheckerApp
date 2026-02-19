5f4632b4465478a10161763e9e5632e4321b638e
ssnehaxxash <snehasunilpathak@gmail.com>
2026-02-19 09:38:52 +0530

    Checking for uc2

--- src/palindromeapp.java
+++ src/palindromeapp.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+public class palindromeapp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version 1.0");
-        System.out.println("System initialized successfully");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        if (word.equalsIgnoreCase(reverse)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+
+        sc.close();
     }
 }
